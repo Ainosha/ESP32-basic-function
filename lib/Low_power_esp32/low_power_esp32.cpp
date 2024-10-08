9f3417410a03e29f44fe2bd3b8be14fa197e6af2
Thomas <segardthomas78@gmail.com>
2024-10-08 09:10:29 +0200

    Fix Low power mode

--- lib/Low_power_esp32/low_power_esp32.cpp
+++ lib/Low_power_esp32/low_power_esp32.cpp
@@ -34,7 +34,7 @@ void int_blink(int sleep_mode)
 void simple_blink() 
 {
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      ,
+  delay(1000);       
   // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   delay(1000);                      // wait for a second
--- lib/Low_power_esp32/low_power_esp32.h
+++ lib/Low_power_esp32/low_power_esp32.h
@@ -1,5 +1,7 @@
-#ifndef LOW_POWER_ESP32.H
-#define LOW_POWER_ESP32.H
+#ifndef LOW_POWER_ESP32_H
+#define LOW_POWER_ESP32_H
+
+#include <Arduino.h>
 
 void int_blink(int sleep_mode);
 void simple_blink();
